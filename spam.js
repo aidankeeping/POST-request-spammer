ee0609f640216df07b3202896eca5b5e0ba719c9
aidankeeping <45411633+aidankeeping@users.noreply.github.com>
2019-08-16 10:44:29 -0700

    Got rid of unneeded variable declarations.

--- spam.js
+++ spam.js
@@ -1,9 +1,6 @@
 const sa = require('superagent');
 const config = require('./config.json');
 setInterval(() => {
-    const url = '';
-    const data = {};
-
     sa.post(config.url).send(config.data).end((err, {body}) => {
       if(err) console.log(err);
     });
